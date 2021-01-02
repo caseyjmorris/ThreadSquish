8a2e98aba9ce509d0192b93906cf18c63c083d9f
Casey J. Morris <caseyjmorris@users.noreply.github.com>
2021-01-02 16:29:40 -0500

    Add test.

--- scripts/runner_test.go
+++ scripts/runner_test.go
@@ -1,7 +1,10 @@
 package scripts
 
 import (
+	"bufio"
+	"bytes"
 	"os/exec"
+	"strconv"
 	"sync"
 	"testing"
 )
@@ -34,5 +37,35 @@ func TestRunner_tryLock(t *testing.T) {
 	err = runner.tryLock()
 	if err != nil {
 		t.Errorf("failed to get second lock:  %s", err)
+	}
+}
+
+func TestRunner_runScriptWithCommander(t *testing.T) {
+	commander := TestCommander{}
+	runner := Runner{}
+	var b bytes.Buffer
+	writer := bufio.NewWriter(&b)
+	var targets []string
+	var excluded []string
+	for i := 0; i < 50; i++ {
+		targets = append(targets, strconv.Itoa(i))
+		if i > 25 {
+			excluded = append(excluded, strconv.Itoa(i))
+		}
+	}
+	err := runner.runScriptWithCommander(16, "c:\\users\\my user\\script.cmd", targets, []string{"a", "b", "c"}, writer, &commander)
+
+	if err != nil {
+		t.Errorf("failed running script:  %s", err)
+		return
+	}
+
+	for i := 0; i < 50; i++ {
+		str := strconv.Itoa(i)
+		_, ok := commander.Targets.Load(str)
+		if i > 25 == ok {
+			t.Errorf("unexpected inclusion value %t for %q", ok, str)
+			return
+		}
 	}
 }
